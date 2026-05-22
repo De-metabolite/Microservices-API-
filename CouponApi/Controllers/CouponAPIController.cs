87014044883e387da17da2efdff34882ccbae842
Bello Qudus <belloqudus3@gmail.com>
2026-05-22 00:53:48 -0700

    Initialcommit

---
+++ CouponApi/Controllers/CouponAPIController.cs
@@ -0,0 +1,54 @@
+﻿using CouponApi.Data;
+using CouponApi.Models;
+using CouponApi.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CouponApi.Controllers
+{
+    [Route("api/CouponAPI")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private readonly ResponseDto _response;
+        public CouponAPIController(AppDbContext db)
+        {
+           _db=db; 
+            _response=new ResponseDto();
+        }
+        [HttpGet]
+        public ResponseDto Get() 
+        {
+            try
+            {
+                IEnumerable<Coupon> objlist = _db.Coupons.ToList();
+                _response.Result = objlist;
+              
+            }catch(Exception ex)
+            {
+                _response.Message = ex.Message;
+                _response.IsSuccess = false;
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get([FromRoute] int id)
+        {
+            try
+            {
+                Coupon objlist = _db.Coupons.First(u=>u.CouponId==id);
+                _response.Result= objlist;
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message;
+                _response.IsSuccess = false;
+            }
+            return _response;
+        }
+
+    }
+}
---
+++ CouponApi/Program.cs
@@ -0,0 +1,44 @@
+using CouponApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(ConnectionString);
+}
+);
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
